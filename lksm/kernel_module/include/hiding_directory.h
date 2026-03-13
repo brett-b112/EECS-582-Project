c566350489f6663bb0ad9c7b4bb0b7f3e235c36e
dustinle721 <harrison.dustin.le@gmail.com>
2026-03-13 10:22:21 -0500

    new changes (my last one until doing other aspects)

---
+++ lksm/kernel_module/include/hiding_directory.h
@@ -0,0 +1,10 @@
+#ifndef HIDING_DIRECTORY_H
+#define HIDING_DIRECTORY_H
+
+#include <linux/ftrace.h>
+int hiding_directory_init(void)
+
+
+
+void hiding_directory_exit(void)
+#endif /* BPF_HOOK_DETECTOR_H */\
