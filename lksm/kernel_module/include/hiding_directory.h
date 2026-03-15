92aae0448337330f9c9e5f8cb6692251ec3ad40a
dustinle721 <harrison.dustin.le@gmail.com>
2026-03-15 09:32:04 -0500

    added comments

--- lksm/kernel_module/include/hiding_directory.h
+++ lksm/kernel_module/include/hiding_directory.h
@@ -2,9 +2,14 @@
 #define HIDING_DIRECTORY_H
 
 #include <linux/ftrace.h>
-int hiding_directory_init(void)
+
+// Initiates the hiding_directory module. This will return 0 for empty or -1 if there is a file in path 
+// int hiding_directory_init(void)
 
 
+// Cleans up and unloads the hiding directory module 
+
+// The function will reverse the behavior done in hiding_directory_init()
 
 void hiding_directory_exit(void)
-#endif /* BPF_HOOK_DETECTOR_H */\
+#endif /* HIDING_DIRECTORY_H */\
